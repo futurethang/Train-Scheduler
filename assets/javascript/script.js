af478d813a23d7d1d4ec5de89b147f5cc457de23
Kevin <kphyde@gmail.com>
2018-09-19 13:29:38 -0700

    begin building with more structured notes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,50 +1,44 @@
-$(function () {
-    $("#test").html("<h1>FARTS</h1>");
-});
+///////// INPUT VARIABLES
+var $train_name = $("#train-name");
+var $train_origin = $("#train_origin"); // Not yet defined in the HTML
+var $destination = $("#destination");
+var $depart_time = $("#depart_time");
+var travel_time = $("#travel_time"); // not yet defined in the HTML
+var $frequency = $("#frequency");
 
+////////// OUTPUT VARIABLES
+// defined in the loop that writes new table rows on form submit (see below)
+
+///// DATABASE VARIABLES
+var trains = []; // an array of train objects based on constructor
 var database = firebase.database();
 
-var $train_name = $("#train-name")
-var $destination = $("#destination")
-var $depart_time = $("#depart_time")
-var $frequency = $("#frequency")
-var idCounter;
-
-// trains stored in object structure like so
-var trainDatabase = {
-    train01: {
-        name: train_name,
-        destination: destination,
-        depart_time: depart_time,
-        frequency: frequency
-        next_arrival: depart_time + travel_time,
-    },
-    train02: {
-        name: train_name,
-        destination: destination,
-        depart_time: depart_time,
-        frequency: frequency
-    },
-    train03: {
-        name: train_name,
-        destination: destination,
-        depart_time: depart_time,
-        frequency: frequency
-    }
-}
-
-function train(idCounter, name, destination, depart_time, frequency) {
-    this.id = idCounter;
+/////////// TRAIN CONSTRUCTOR
+function train(name, origin, destination, depart_time, travel_time, frequency) {
     this.name = name;
+    this.origin = origin;
     this.destination = destination;
     this.depart_time = depart_time;
+    this.travel_time = travel_time;
     this.frequency = frequency;
 
-    calculateArrivalTime = function () { };
+    calculateArrivalTime = function () {
+        return this.depart_time + this.travel_time;
+    };
 
-    calculateMinutesAway = function () { };
+    calculateMinutesAway = function () {
+        
+    };
 
 }
+
+/////////// HELPER FUNCTIONS
+function checkForDuplicateTrain(newTrain) {
+    if (trainArray.contains(newtrain)) { // the key thing I want to check is if the origin and destination are same
+        alert("That route already exists")
+    } else {trains.push(newTrain)} // Add the new train to the trains array
+}
+
 
 // when a train's start time passes, a new instance begins to time down its 'minutes away'
 // so a train has its leave interval, one new created each XX minutes, take the current time stamp and add travel time
@@ -67,14 +61,32 @@ database.ref().on("value", function (snapshot) {
         var newColumn = $("<td>");
                 
         for (let i = 0; i < trainArray.length; i++) {
-            var trainName = "<td>" + snapshot.val()[i].trainName + "</td>";
-            var trainDestination = "<td>" + snapshot.val()[i].trainDestination + "</td>";
-            var trainFrequency = "<td>" + snapshot.val()[i].trainFrequency + "</td>";
-            var trainArrival = "<td>" + snapshot.val()[i].trainArrival + "</td>";
-            var trainMinAway = "<td>" + snapshot.val()[i].trainMinAway + "</td>";
+            trainName = "<td>" + snapshot.val()[i].trainName + "</td>";
+            trainDestination = "<td>" + snapshot.val()[i].trainDestination + "</td>";
+            trainFrequency = "<td>" + snapshot.val()[i].trainFrequency + "</td>";
+            trainArrival = "<td>" + snapshot.val()[i].trainArrival + "</td>";
+            trainMinAway = "<td>" + snapshot.val()[i].trainMinAway + "</td>";
 
             $("#train_schedule").append(newRow).html(trainName + trainDestination + trainFrequency + trainArrival + trainMinAway);
-
         }
     }
+    // Else Firebase doesn't have a Train, so use the initial local values.
+    else {} // write in the state for only local info shown
 }
+
+$("#submit").on("click", function(e) {  
+    e.preventDefault();
+
+    var trainName = $("#train_name").val().trim();
+    var trainDestination = $("#destination").val().trim();
+    var trainFrequency = $("frequency").val().trim();
+    var trainDepartTime = $("#depart_time").val().trim();
+
+    database.ref().set({
+        name: trainName,
+        destination: trainDestination,
+        depart_time: trainDepartTime,
+        frequency: trainFrequency,
+        next_arrival: depart_time + travel_time //undefined values
+    })
+})
